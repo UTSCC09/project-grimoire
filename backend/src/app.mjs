f6dd7dacf4f761bc7f5ac2fd7f21f28db9cc70e1
Mall0-w <kylelewis@rogers.com>
2023-11-08 15:56:15 -0500

    introduced api call for local game stores
    TODO: strip down api call so that doesn't return unneeded info

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -11,6 +11,7 @@ import disRouter from "./deathInSpace/routes.mjs";
 import { readFileSync } from "fs";
 import sheetRouter from "./genericSheets/routes.mjs";
 import { sendEmail, sendValidationEmail } from "./aws/ses_helper.mjs";
+import { MapsRouter } from "./googleMaps/routes.mjs";
 
 dotenv.config();
 
@@ -57,6 +58,8 @@ app.use(function (req, res, next) {
 app.use('/api/dis', disRouter)
 
 app.use('/api/sheets', sheetRouter)
+
+app.use('/api/maps', MapsRouter)
 
 /**
  * sanity check endpoint to test connection
