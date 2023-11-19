77bed2140be30aad419438aa63f337cf7ace8950
Mall0-w <kylelewis@rogers.com>
2023-11-19 09:35:50 -0500

    added reverse geocoding endpoint

--- backend/src/googleMaps/gMaps.mjs
+++ backend/src/googleMaps/gMaps.mjs
@@ -17,13 +17,56 @@ export function getLocalGames(lat, lng, radius=DEFAULTGAMERADIUS){
     )
 }
 
-export function testGeocode(){
-    console.log('api key', API_KEY)
-    const args = {
-        params: {
-            key: API_KEY,
-            address: 'Perth 4WD & Commercial Centre',
+//make sure to look at documentation for allowed result_types
+//https://developers.google.com/maps/documentation/geocoding/requests-reverse-geocoding
+//https://googlemaps.github.io/google-maps-services-js/enums/PlaceType2.html
+export function reverseGeocode(lat, lng, result_types){
+    if(!result_types)
+        result_types = ['country', 'administrative_area_level_1',
+    "administrative_area_level_2", "locality"]
+    //     result_types = ['country', 'administrative_area_level_1', 
+    // 'administrative_area_level_2 ', 'sublocality']
+    return client.reverseGeocode({
+        params:{
+            latlng: [lat, lng],
+            result_type: result_types,
+            key: API_KEY
         }
-    };
-    return client.geocode(args)
+    })
 }
+
+//function that formats geoocde response to be more useful
+//reference documentation for types
+export function formatReverseGeocode(geocodeResults, preferredIndex=0){
+    if(geocodeResults.length <= preferredIndex)
+        return {}
+    let geocodeObj = geocodeResults[preferredIndex]
+    
+    const components = geocodeObj.address_components
+    if(!components)
+        return {}
+    let formattedObj = {}
+    for(let comp of components){
+        let newObj = {
+            short_name: comp.short_name,
+            long_name: comp.long_name
+        }
+        let filtered = comp.types.filter((t) => t != 'political')
+        let key = filtered.length > 0 ? filtered[0] : 'political'
+        formattedObj[key] = newObj
+    }
+
+    geocodeObj.address_components = formattedObj
+    return geocodeObj
+}
+
+// export function testGeocode(){
+//     console.log('api key', API_KEY)
+//     const args = {
+//         params: {
+//             key: API_KEY,
+//             address: 'Perth 4WD & Commercial Centre',
+//         }
+//     };
+//     return client.geocode(args)
+// }
